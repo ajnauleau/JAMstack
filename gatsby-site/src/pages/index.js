b1116c796f90059ec8956b985f076bc89c0a3cce
Antoine Nauleau <ajnauleau@gmail.com>
2018-08-30 11:22:18 -0700

    JAMstack hackathon landing page- under construction

---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>JAMstack Hackathon</h1>
+    <p>Welcome to my hackathon projects landing page.</p>
+    <p>Were going to build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
