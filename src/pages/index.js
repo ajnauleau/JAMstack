321ad9f909afdec77300232fd51b595b1eaa90bd
Antoine Nauleau <ajnauleau@gmail.com>
2018-08-30 11:54:11 -0700

    First deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>JAMstack Hackathon</h1>
     <p>Welcome to my hackathon projects landing page.</p>
     <p>Were going to build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/app/">Go to the App</Link>
   </div>
 )
 
